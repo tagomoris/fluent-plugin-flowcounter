0dc6c09cc62e4f8a1cd574789ab445e58dbdfec2
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-02-02 13:00:48 +0900

    replace files

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-flowcounter'
+require 'fluent/plugin/out_flowcounter'
 
 class Test::Unit::TestCase
 end
